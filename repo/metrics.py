7fd8f77b8a8edea1cbce14a67f42224c46de8cc4
lzvtlshkvch <47631604+lzvtlshkvch@users.noreply.github.com>
2023-10-15 21:25:53 +0300

    Update metrics.py

--- repo/metrics.py
+++ repo/metrics.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from scipy.spatial.distance import cdist, pdist
 from scipy.spatial.distance import _validate_vector
-from scipy.stats import median_absolute_deviation
+from scipy.stats import median_abs_deviation
 
 from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
@@ -105,7 +105,7 @@ def mad_cityblock(u, v, mad):
 def continuous_distance(x, cf_list, continuous_features, metric='euclidean', X=None, agg=None):
 
     if metric == 'mad':
-        mad = median_absolute_deviation(X[:, continuous_features], axis=0)
+        mad = median_abs_deviation(X[:, continuous_features], axis=0)
         mad = np.array([v if v != 0 else 1.0 for v in mad])
 
         def _mad_cityblock(u, v):
