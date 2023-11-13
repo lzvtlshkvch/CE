dcd8e24eb7e9dda282eb48a701e9f8a0cc02ff90
lzvtlshkvch <47631604+lzvtlshkvch@users.noreply.github.com>
2023-11-13 23:15:29 +0300

    Update metrics.py

--- repo/metrics.py
+++ repo/metrics.py
@@ -9,29 +9,10 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.preprocessing import StandardScaler
 
-
-class DummyScaler:
-
-    def __init__(self):
-        pass
-
-    def fit(self, X):
-        pass
-
-    def fit_transform(self, X):
-        return X
-
-    def transform(self, X):
-        return X
-
-    def inverse_transform(self, X):
-        return X
-
-
 def nbr_valid_cf(cf_list, b, y_val, y_desidered=None):
     y_cf = b.predict(cf_list)
     idx = y_cf != y_val if y_desidered is None else y_cf == y_desidered
-    val = np.sum(idx)
+    val = idx*1
     return val
 
 
@@ -40,86 +21,6 @@ def perc_valid_cf(cf_list, b, y_val, k=None, y_desidered=None):
     k = len(cf_list) if k is None else k
     res = n_val / k
     return res
-
-
-def nbr_actionable_cf(x, cf_list, variable_features):
-    nbr_actionable = 0
-    nbr_features = cf_list.shape[1]
-    for i, cf in enumerate(cf_list):
-        constraint_violated = False
-        for j in range(nbr_features):
-            if cf[j] != x[j] and j not in variable_features:
-                constraint_violated = True
-                break
-        if not constraint_violated:
-            nbr_actionable += 1
-
-    return nbr_actionable
-
-
-def perc_actionable_cf(x, cf_list, variable_features, k=None):
-    n_val = nbr_actionable_cf(x, cf_list, variable_features)
-    k = len(cf_list) if k is None else k
-    res = n_val / k
-    return res
-
-
-def nbr_valid_actionable_cf(x, cf_list, b, y_val, variable_features, y_desidered=None):
-    y_cf = b.predict(cf_list)
-    idx = y_cf != y_val if y_desidered is None else y_cf == y_desidered
-
-    nbr_valid_actionable = 0
-    nbr_features = cf_list.shape[1]
-    for i, cf in enumerate(cf_list):
-        if not np.array(idx)[i]:
-            continue
-        constraint_violated = False
-        for j in range(nbr_features):
-            if cf[j] != x[j] and j not in variable_features:
-                constraint_violated = True
-                break
-        if not constraint_violated:
-            nbr_valid_actionable += 1
-
-    return nbr_valid_actionable
-
-
-def perc_valid_actionable_cf(x, cf_list, b, y_val, variable_features, k=None, y_desidered=None):
-    n_val = nbr_valid_actionable_cf(x, cf_list, b, y_val, variable_features, y_desidered)
-    k = len(cf_list) if k is None else k
-    res = n_val / k
-    return res
-
-
-def nbr_violations_per_cf(x, cf_list, variable_features):
-    nbr_features = cf_list.shape[1]
-    nbr_violations = np.zeros(len(cf_list))
-    for i, cf in enumerate(cf_list):
-        for j in range(nbr_features):
-            if cf[j] != x[j] and j not in variable_features:
-                nbr_violations[i] += 1
-    return nbr_violations
-
-
-def avg_nbr_violations_per_cf(x, cf_list, variable_features):
-    return np.mean(nbr_violations_per_cf(x, cf_list, variable_features))
-
-def std_nbr_violations_per_cf(x, cf_list, variable_features):
-    return np.std(nbr_violations_per_cf(x, cf_list, variable_features))
-
-def avg_nbr_violations(x, cf_list, variable_features):
-    val = np.sum(nbr_violations_per_cf(x, cf_list, variable_features))
-    nbr_cf, nbr_features = cf_list.shape
-    return val / (nbr_cf * nbr_features)
-
-
-def mad_cityblock(u, v, mad):
-    u = _validate_vector(u)
-    v = _validate_vector(v)
-    l1_diff = abs(u - v)
-    l1_diff_mad = l1_diff / mad
-    return l1_diff_mad.sum()
-
 
 def continuous_distance(x, cf_list, continuous_features, metric='euclidean', X=None, agg=None):
 
@@ -163,59 +64,48 @@ def categorical_distance(x, cf_list, categorical_features, metric='jaccard', agg
         return np.std(dist)
 
 
-def distance_l2j(x, cf_list, continuous_features, categorical_features, ratio_cont=None, agg=None):
-    nbr_features = cf_list.shape[1]
-    dist_cont = continuous_distance(x, cf_list, continuous_features, metric='euclidean', X=None, agg=agg)
-    dist_cate = categorical_distance(x, cf_list, categorical_features, metric='jaccard', agg=agg)
-    if ratio_cont is None:
-        ratio_continuous = len(continuous_features) / nbr_features
-        ratio_categorical = len(categorical_features) / nbr_features
-    else:
-        ratio_continuous = ratio_cont
-        ratio_categorical = 1.0 - ratio_cont
-    dist = ratio_continuous * dist_cont + ratio_categorical * dist_cate
-    return dist
-
-
-def distance_mh(x, cf_list, continuous_features, categorical_features, X, ratio_cont=None, agg=None):
-    nbr_features = cf_list.shape[1]
-    dist_cont = continuous_distance(x, cf_list, continuous_features, metric='mad', X=X, agg=agg)
-    dist_cate = categorical_distance(x, cf_list, categorical_features, metric='hamming', agg=agg)
-    if ratio_cont is None:
-        ratio_continuous = len(continuous_features) / nbr_features
-        ratio_categorical = len(categorical_features) / nbr_features
-    else:
-        ratio_continuous = ratio_cont
-        ratio_categorical = 1.0 - ratio_cont
-    dist = ratio_continuous * dist_cont + ratio_categorical * dist_cate
-    return dist
-
-
-# basso e' meglio cambi con x
-def nbr_changes_per_cf(x, cf_list, continuous_features):
+def nbr_changes_per_cf(x, cf_list, variable_features):
     nbr_features = cf_list.shape[1]
     nbr_changes = np.zeros(len(cf_list))
     for i, cf in enumerate(cf_list):
         for j in range(nbr_features):
             if cf[j] != x[j]:
-                nbr_changes[i] += 1 if j in continuous_features else 0.5
+                nbr_changes[i] += 1 if j in variable_features else 0.5
     return nbr_changes
 
 
-def avg_nbr_changes_per_cf(x, cf_list, continuous_features):
-    return np.mean(nbr_changes_per_cf(x, cf_list, continuous_features))
+def avg_nbr_changes_per_cf(x, cf_list, variable_features):
+    return np.mean(nbr_changes_per_cf(x, cf_list, variable_features))
     
-def std_nbr_changes_per_cf(x, cf_list, continuous_features):
-    return np.std(nbr_changes_per_cf(x, cf_list, continuous_features))
+def std_nbr_changes_per_cf(x, cf_list, variable_features):
+    return np.std(nbr_changes_per_cf(x, cf_list, variable_features))
 
+def distance_l2(x, cf_list, continuous_features, metric='euclidean', X=None, agg=None):
 
-def avg_nbr_changes(x, cf_list, nbr_features, continuous_features):
-    val = np.sum(nbr_changes_per_cf(x, cf_list, continuous_features))
-    nbr_cf, _ = cf_list.shape
-    return val / (nbr_cf * nbr_features)
+    if metric == 'mad':
+        mad = median_abs_deviation(X[:, continuous_features], axis=0)
+        mad = np.array([v if v != 0 else 1.0 for v in mad])
 
+        def _mad_cityblock(u, v):
+            return mad_cityblock(u, v, mad)
+        dist = cdist(x.reshape(1, -1)[:, continuous_features], cf_list[:, continuous_features], metric=_mad_cityblock)
+    else:
+        dist = cdist(x.reshape(1, -1)[:, continuous_features], cf_list[:, continuous_features], metric=metric)
 
-def continuous_diversity(cf_list, continuous_features, metric='euclidean', X=None, agg=None):
+    if agg is None or agg == 'mean':
+        return np.mean(dist)
+
+    if agg == 'max':
+        return np.max(dist)
+
+    if agg == 'min':
+        return np.min(dist)
+
+    if agg == 'std':
+        return np.std(dist)
+        
+
+def diversity_l2(cf_list, continuous_features, metric='euclidean', X=None, agg=None):
     if metric == 'mad':
         mad = median_abs_deviation(X[:, continuous_features], axis=0)
         mad = np.array([v if v != 0 else 1.0 for v in mad])
@@ -237,194 +127,6 @@ def continuous_diversity(cf_list, continuous_features, metric='euclidean', X=Non
         
     if agg == 'std':
         return np.std(dist)
-
-
-def categorical_diversity(cf_list, categorical_features, metric='jaccard', agg=None):
-
-    dist = pdist(cf_list[:, categorical_features], metric=metric)
-
-    if agg is None or agg == 'mean':
-        return np.mean(dist)
-
-    if agg == 'max':
-        return np.max(dist)
-
-    if agg == 'min':
-        return np.min(dist)
-        
-    if agg == 'std':
-        return np.std(dist)
-
-
-def diversity_l2j(cf_list, continuous_features, categorical_features, ratio_cont=None, agg=None):
-    nbr_features = cf_list.shape[1]
-    dist_cont = continuous_diversity(cf_list, continuous_features, metric='euclidean', X=None, agg=agg)
-    dist_cate = categorical_diversity(cf_list, categorical_features, metric='jaccard', agg=agg)
-    if ratio_cont is None:
-        ratio_continuous = len(continuous_features) / nbr_features
-        ratio_categorical = len(categorical_features) / nbr_features
-    else:
-        ratio_continuous = ratio_cont
-        ratio_categorical = 1.0 - ratio_cont
-    dist = ratio_continuous * dist_cont + ratio_categorical * dist_cate
-    return dist
-
-
-def diversity_mh(cf_list, continuous_features, categorical_features, X, ratio_cont=None, agg=None):
-    nbr_features = cf_list.shape[1]
-    dist_cont = continuous_diversity(cf_list, continuous_features, metric='mad', X=X, agg=agg)
-    dist_cate = categorical_diversity(cf_list, categorical_features, metric='hamming', agg=agg)
-    if ratio_cont is None:
-        ratio_continuous = len(continuous_features) / nbr_features
-        ratio_categorical = len(categorical_features) / nbr_features
-    else:
-        ratio_continuous = ratio_cont
-        ratio_categorical = 1.0 - ratio_cont
-    dist = ratio_continuous * dist_cont + ratio_categorical * dist_cate
-    return dist
-
-
-def count_diversity(cf_list, features, nbr_features, continuous_features):
-    nbr_cf = cf_list.shape[0]
-    nbr_changes = 0
-    for i in range(nbr_cf):
-        for j in range(i+1, nbr_cf):
-            for k in features:
-                if cf_list[i][k] != cf_list[j][k]:
-                    nbr_changes += 1 if j in continuous_features else 0.5
-    return nbr_changes / (nbr_cf * nbr_cf * nbr_features)
-
-
-# piu alto e' meglio conta variet' tra cf
-def count_diversity_all(cf_list, nbr_features, continuous_features):
-    return count_diversity(cf_list, range(cf_list.shape[1]), nbr_features, continuous_features)
-
-
-def euclidean_jaccard(x, A, continuous_features, categorical_features, ratio_cont=None):
-    nbr_features = A.shape[1]
-    dist_cont = cdist(x.reshape(1, -1)[:, continuous_features], A[:, continuous_features], metric='euclidean')
-    dist_cate = cdist(x.reshape(1, -1)[:, categorical_features], A[:, categorical_features], metric='jaccard')
-    if ratio_cont is None:
-        ratio_continuous = len(continuous_features) / nbr_features
-        ratio_categorical = len(categorical_features) / nbr_features
-    else:
-        ratio_continuous = ratio_cont
-        ratio_categorical = 1.0 - ratio_cont
-    dist = ratio_continuous * dist_cont + ratio_categorical * dist_cate
-    return dist
-
-
-def accuracy_knn_sklearn(x, cf_list, b, X_test, continuous_features, categorical_features, scaler, test_size=5):
-    clf = KNeighborsClassifier(n_neighbors=1)
-    X_train = np.vstack([x.reshape(1, -1), cf_list])
-    y_train = b.predict(X_train)
-    clf.fit(X_train, y_train)
-
-    X_test_knn = select_test_knn(x, b, X_test, continuous_features, categorical_features, scaler, test_size)
-    y_test = b.predict(X_test_knn)
-    y_pred = clf.predict(X_test_knn)
-
-    return accuracy_score(y_test, y_pred)
-
-
-def accuracy_knn_dist(x, cf_list, b, X_test, continuous_features, categorical_features, scaler, test_size=5):
-
-    X_train = np.vstack([x.reshape(1, -1), cf_list])
-    y_train = b.predict(X_train)
-
-    nX_train = scaler.transform(X_train)
-
-    X_test_knn, nX_test_knn = select_test_knn(x, b, X_test, continuous_features, categorical_features, scaler,
-                                              test_size, get_normalized=True)
-    y_test = b.predict(X_test_knn)
-
-    y_pred = list()
-    for nx_test in nX_test_knn:
-        dist = euclidean_jaccard(nx_test, nX_train, continuous_features, categorical_features)
-        idx = np.argmin(dist)
-        y_pred.append(y_train[idx])
-
-    return accuracy_score(y_test, y_pred)
-
-
-def select_test_knn(x, b, X_test, continuous_features, categorical_features, scaler, test_size=5, get_normalized=False):
-    y_val = b.predict(x.reshape(1, -1))
-    y_test = b.predict(X_test)
-    nx = scaler.transform(x.reshape(1, -1))
-    nX_test = scaler.transform(X_test)
-    dist_f = euclidean_jaccard(nx, nX_test[y_test == y_val], continuous_features, categorical_features)
-    dist_cf = euclidean_jaccard(nx, nX_test[y_test != y_val], continuous_features, categorical_features)
-    index_f = np.argsort(dist_f)[0][:test_size].tolist()
-    index_cf = np.argsort(dist_cf)[0][:test_size].tolist()
-    index = np.array(index_f + index_cf)
-    if get_normalized:
-        return X_test[index], nX_test[index]
-    return X_test[index]
-
-
-def lof(x, cf_list, X, scaler):
-    X_train = np.vstack([x.reshape(1, -1), X])
-
-    nX_train = scaler.transform(X_train)
-    ncf_list = scaler.transform(cf_list)
-
-    clf = LocalOutlierFactor(n_neighbors=3, novelty=True)
-    clf.fit(nX_train)
-
-    lof_values = clf.predict(ncf_list)
-    return np.mean(np.abs(lof_values))
-
-# differenza in predict proba con x -> piu alto e' megliio
-def delta_proba(x, cf_list, b, agg=None):
-    y_val = b.predict(x.reshape(1, -1))
-    y_cf = b.predict(cf_list)
-    deltas = np.abs(y_cf - y_val)
-
-    if agg is None or agg == 'mean':
-        return np.mean(deltas)
-
-    if agg == 'max':
-        return np.max(deltas)
-
-    if agg == 'min':
-        return np.min(deltas)
-
-    if agg == 'std':
-        return np.std(deltas)
-
-
-def delta_proba_img_ts(x, cf_list, b, agg=None):
-    y_val = b.predict(x.reshape((1,) + x.shape))
-    y_cf = np.array([b.predict(cf.reshape((1,) + cf.shape)) for cf in cf_list])
-    deltas = np.abs(y_cf - y_val)
-
-    if agg is None or agg == 'mean':
-        return np.mean(deltas)
-
-    if agg == 'max':
-        return np.max(deltas)
-
-    if agg == 'min':
-        return np.min(deltas)
-
-
-def plausibility(x, bb, cf_list, X_test, y_pred, continuous_features_all,
-                 categorical_features_all, X_train, ratio_cont):
-    sum_dist = 0.0
-    for cf in cf_list:
-        y_cf_val = bb.predict(cf.reshape(1, -1))[0]
-        X_test_y = X_test[y_cf_val == y_pred]
-        # neigh_dist = exp.cdist(x.reshape(1, -1), X_test_y)
-        neigh_dist = distance_mh(x.reshape(1, -1), X_test_y, continuous_features_all,
-                        categorical_features_all, X_train, ratio_cont)
-        idx_neigh = np.argsort(neigh_dist)[0]
-        # closest_idx = closest_idx = idx_neigh[0]
-        # closest = X_test_y[closest_idx]
-        closest = X_test_y[idx_neigh]
-        d = distance_mh(cf, closest.reshape(1, -1), continuous_features_all,
-                        categorical_features_all, X_train, ratio_cont)
-        sum_dist += d
-    return sum_dist
 
 def plausibility_domain(cf_list, X_test, X_train, variable_features):
     nbr_plausibility = 0
